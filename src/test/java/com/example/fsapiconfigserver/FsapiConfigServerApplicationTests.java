bf0714d1df871b0ab3f8ce483cae44bda1b0e259
schennamsetty-incomm <48800191+schennamsetty-incomm@users.noreply.github.com>
2022-11-09 16:06:04 -0500

    Commiting the files

---
+++ src/test/java/com/example/fsapiconfigserver/FsapiConfigServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.fsapiconfigserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FsapiConfigServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
